261b51780745dd45ca90cd23570a9a335d7b69f0
Mikael <mika.marcondes2@gmail.com>
2024-09-30 12:28:23 -0300

    Adjust grid size

--- App.tsx
+++ App.tsx
@@ -1,40 +1,50 @@
-import {StyleSheet} from 'react-native';
-import {GestureHandlerRootView} from "react-native-gesture-handler";
-import Touchable from "react-native-skia-gesture"
+import { StyleSheet, useWindowDimensions } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Touchable from "react-native-skia-gesture";
 
 export default function App() {
-    const recWidth = 80;
-    const recHeight = 80;
-    const recContainer = 90;
+  const { width: windowWidth, height: windowHeight } = useWindowDimensions();
+  const padding = 20;
 
-    const gridSize = 300;
-    const rowIndex = 3;
-    const colIndex = 3;
+  const gridSize = windowWidth - padding;
 
-    return (
-        <GestureHandlerRootView style={styles.container}>
-            <Touchable.Canvas style={{width: gridSize, height: gridSize, backgroundColor: 'grey'}}>
-                {new Array(rowIndex).fill(0).map((_, i) => {
-                    return new Array(colIndex).fill(0).map((_, j) => {
-                        return <Touchable.Rect
-                            x={i * recContainer}
-                            y={j * recContainer}
-                            width={recWidth}
-                            height={recHeight}
-                            color="lightgrey"
-                        />
-                    })
-                })}
-            </Touchable.Canvas>
+  const rowIndex = 3;
+  const colIndex = 3;
 
-        </GestureHandlerRootView>
-    );
+  const recWidth = gridSize / rowIndex;
+  const recHeight = gridSize / colIndex;
+
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <Touchable.Canvas
+        style={{
+          width: gridSize,
+          height: gridSize,
+          backgroundColor: "grey",
+        }}
+      >
+        {new Array(rowIndex).fill(0).map((_, i) => {
+          return new Array(colIndex).fill(0).map((_, j) => {
+            return (
+              <Touchable.Rect
+                x={i * recWidth}
+                y={j * recHeight}
+                width={recWidth}
+                height={recHeight}
+                color="lightgrey"
+              />
+            );
+          });
+        })}
+      </Touchable.Canvas>
+    </GestureHandlerRootView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
