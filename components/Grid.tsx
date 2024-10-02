e9f5b3ab7473bc3a94ebb091e1fea74f16bf5a53
Mikael <mika.marcondes2@gmail.com>
2024-10-02 11:39:46 -0300

    Draw grid lines on top

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,14 +1,22 @@
 import Touchable from "react-native-skia-gesture";
 import { useWindowDimensions } from "react-native";
+import { Group, Paint, Rect } from "@shopify/react-native-skia";
+import { useMemo } from "react";
 
 export const Grid = () => {
   const { width: windowWidth, height: windowHeight } = useWindowDimensions();
 
   const padding = 20;
+
   const rowIndex = 3;
   const colIndex = 3;
 
   const gridSize = windowWidth - padding;
+  const gridStroke = 4;
+  const gridCellColor = "transparent";
+  const gridStrokeColor = "black";
+  const gridOffset = -10;
+  const gridOverlaySize = 500;
 
   const recWidth = gridSize / rowIndex;
   const recHeight = gridSize / colIndex;
@@ -18,7 +26,6 @@ export const Grid = () => {
       style={{
         width: gridSize,
         height: gridSize,
-        backgroundColor: "grey",
       }}
     >
       {new Array(rowIndex).fill(0).map((_, i) => {
@@ -29,11 +36,39 @@ export const Grid = () => {
               y={j * recHeight}
               width={recWidth}
               height={recHeight}
-              color="lightgrey"
+              color={"lightgrey"}
             />
           );
         });
       })}
+      <Group>
+        <Rect
+          width={recWidth}
+          height={gridOverlaySize}
+          x={gridSize / colIndex}
+          y={gridOffset}
+          color={gridCellColor}
+        >
+          <Paint
+            color={gridStrokeColor}
+            style={"stroke"}
+            strokeWidth={gridStroke}
+          ></Paint>
+        </Rect>
+        <Rect
+          width={gridOverlaySize}
+          height={recHeight}
+          x={gridOffset}
+          y={gridSize / colIndex}
+          color={gridCellColor}
+        >
+          <Paint
+            color={gridStrokeColor}
+            style={"stroke"}
+            strokeWidth={gridStroke}
+          ></Paint>
+        </Rect>
+      </Group>
     </Touchable.Canvas>
   );
 };
